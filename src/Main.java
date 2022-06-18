9966d452c2cec8604ef616ce8415a5f128adf3bc
Denver <den-knyazev@mail.ru>
2022-06-18 19:32:07 +0300

    Домашка строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //  Задача 1
+String firstName = "Ivan ";
+String middleName = "Ivanovich ";
+String lastName = "Ivanov ";
+String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+// Задача 2
+       String fullNameBig = fullName.toUpperCase() ;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - "+ fullNameBig);
+        //Задача 3
+        fullName = "Иванов Семён Семёнович";
+        String fullNameReplace = fullName.replace('ё','е');
+        System.out.println("Данные ФИО сотрудника " + fullNameReplace);
+
+
+
     }
 }
