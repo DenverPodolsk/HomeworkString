20ac8cc33c4527b07ea4300cf060dab626352ac9
Denver <den-knyazev@mail.ru>
2022-06-18 20:13:34 +0300

    Домашка строки

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
        //  Задача 1
@@ -11,8 +13,24 @@ String fullName = lastName + firstName + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета - "+ fullNameBig);
         //Задача 3
         fullName = "Иванов Семён Семёнович";
-        String fullNameReplace = fullName.replace('ё','е');
-        System.out.println("Данные ФИО сотрудника " + fullNameReplace);
+   //     String fullNameReplace = fullName.replace('ё','е');
+    //    System.out.println("Данные ФИО сотрудника " + fullNameReplace);
+        System.out.println(Arrays.toString(fullName.split(" ")));
+        String[] a = fullName.split(" ");
+
+        for( int i=0;i<a.length;i++ );{
+            String newString = "";
+            int i =0;
+            for (int b = 0; b<a[i].length(); b++){
+            if (a[i].charAt(b) =='е'){
+                newString +='е';
+            }
+            else
+                newString += a[i].charAt(b);
+            }
+            System.out.println(newString);
+
+            }
 
 
 
